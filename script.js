407e86a4fb8f4a41a40b32151597542af0439301
bashmobarmeg <baashmobarmeg@gmail.com>
2026-05-22 22:43:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,25 @@ document.getElementById("stop").onclick = function () {
         el.classList.toggle("no-shadow");
     });
 };
+
+let elements = document.querySelectorAll(
+    ".Project1, .Project2, .Project3, .visit"
+);
+
+let observer = new IntersectionObserver(function(entries){
+    
+    entries.forEach(function(entry){
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+elements.forEach(function(element){
+    observer.observe(element);
+});
